66be5c18e77211adecc804c0e844efdfc9a59cab
MiguelPalmaWpp <miguelangel.palma@wppmedia.com>
2026-07-15 12:25:35 -0500

    Define null coalescing helper for Connect

--- app_modules/config.R
+++ app_modules/config.R
@@ -1,8 +1,10 @@
 options(shiny.maxRequestSize = 300 * 1024^2)
 
-source("functions.R")
-
 DEFAULT_CUTOFF_DATE <- as.Date("2026-01-31")
 DEFAULT_REVENUE_PER_UNIT <- 30090
 DEFAULT_ROI_FROM <- as.Date("2026-02-01")
 DEFAULT_ROI_TO <- as.Date("2026-12-31")
+
+`%||%` <- function(x, y) {
+  if (is.null(x) || length(x) == 0) y else x
+}
